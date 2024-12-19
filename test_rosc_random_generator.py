bada41ad79a44965713249711a0a977cc198ea39
MicroControleurMonde <MicroMondeElectronique@gmail.com>
2024-12-19 12:27:06 +0300

    Update test_rosc_random_generator.py

--- test_rosc_random_generator.py
+++ test_rosc_random_generator.py
@@ -3,3 +3,6 @@ from rosc_random_generator import ROSCRandomGenerator
 # Initialize and use the ROSC Random Generator
 generator = ROSCRandomGenerator()
 generator.generate_continuous_random_values(32, 10)
+
+# 32 - for 32 bits, you can adjust it to 4 / 8 / 16 / 24 as you wish.
+# 10 - for the number of randon values needed.
